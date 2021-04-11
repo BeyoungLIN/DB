3d807d14df69ba561ba7c34647c7ee4359ae56ee
BeyoungLIN <BeyoungLIN@users.noreply.github.com>
2021-04-11 19:34:51 +0800

    update

--- scripts/run_special_demo_newdb.sh
+++ scripts/run_special_demo_newdb.sh
@@ -3,7 +3,7 @@ export CUDA_VISIBLE_DEVICES=0
 
 python special_demo.py \
 experiments/seg_detector/fakepages0310_resnet50_deform_thre.yaml \
---image_path ../data/DingXiu \
+--image_path /disks/sdb/projs/AncientBooks/data/Dingxiu_test_newdb/0A0CBAE0046F4AB7BCBFE12789547A78 \
 --resume \
 /disks/sdc/projs/AncientBooks/models/db/fakepage_res50_iter3.bin \
 --box_thresh 0.5 \
