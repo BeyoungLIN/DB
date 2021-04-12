5a1bbd3bdf363af3fc01f77afe893d1c35eb13c2
BeyoungLIN <BeyoungLIN@users.noreply.github.com>
2021-04-12 18:32:52 +0800

    update

--- scripts/run_demo.sh
+++ scripts/run_demo.sh
@@ -3,9 +3,9 @@ export CUDA_VISIBLE_DEVICES=0
 
 python demo.py \
 experiments/seg_detector/fakepages_resnet18_deform_thre.yaml \
---image_path /disks/sdb/euphoria/CUHK_OCR/3.22竞赛数据/imgs/image_032_meitu.jpg \
+--image_path /disks/sdb/projs/AncientBooks/data/Dingxiu_test_newdb/0A0D3D1A7FC14756978AF46C4AE2F907/ \
 --visualize \
 --sort_boxes \
 --resume /disks/sdb/euphoria/pkg/seg_detector/models/fakepages_resnet18 \
 --box_thresh 0.5 \
---result_dir /disks/sdb/euphoria/pkg/seg_detector/CUHK_0322_demo_results_res18_old_contrast \
+--result_dir /disks/sdb/euphoria/pkg/seg_detector/Dingxiu_2_demo_results_res18 \
---
+++ scripts/run_demo_newdb.sh
@@ -0,0 +1,12 @@
+
+export CUDA_VISIBLE_DEVICES=0
+
+python demo.py \
+experiments/seg_detector/fakepages0310_resnet50_deform_thre.yaml \
+--image_path /disks/sdb/projs/AncientBooks/data/Dingxiu_test_newdb/0A0D3D1A7FC14756978AF46C4AE2F907/ \
+--visualize \
+--sort_boxes \
+--resume \
+/disks/sdc/projs/AncientBooks/models/db/fakepage_res50_iter3.bin \
+--box_thresh 0.5 \
+--result_dir /disks/sdb/euphoria/pkg/seg_detector/Dingxiu_2_demo_results_newdb \
