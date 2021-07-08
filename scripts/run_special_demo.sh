5e598f98564574cad32895999ebd7aa758d558d0
beyoung <linbeyoung@foxmail.com>
2021-07-08 14:11:50 +0800

    backup

--- scripts/run_special_demo.sh
+++ scripts/run_special_demo.sh
@@ -3,7 +3,7 @@ export CUDA_VISIBLE_DEVICES=1
 
 python special_demo.py \
 experiments/seg_detector/fakepages_resnet18_deform_thre.yaml \
---image_path ../data/DingXiu \
+--image_path /disks/sdb/projs/AncientBooks/data/DingXiu/0A0CBAE0046F4AB7BCBFE12789547A78 \
 --resume \
 models/model_epoch_490_minibatch_396000 \
 --box_thresh 0.5 \
