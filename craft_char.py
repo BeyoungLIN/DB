1adc3a171044c0a398119d767a5d5e49809a98f1
beyoung <linbeyoung@foxmail.com>
2021-08-15 19:06:07 +0800

    update craft_char

--- craft_char.py
+++ craft_char.py
@@ -30,6 +30,14 @@ url_page_recog_0 = 'http://api.chinesenlp.com:7001/ocr/v1/page_recog_0'
 url_page_recog = 'http://api.chinesenlp.com:7001/ocr/v1/page_recog'
 url_page_recog_1 = 'http://api.chinesenlp.com:7001/ocr/v1/page_recog_1'
 
+def cv_imread(file_path=""):
+    img_mat = cv2.imdecode(np.fromfile(file_path, dtype=np.uint8), cv2.IMREAD_COLOR)
+    return img_mat
+
+
+def cv_imwrite(file_path, frame):
+    cv2.imencode('.jpg', frame)[1].tofile(file_path)
+
 def base64of_img(pth_img):
     image_base64 = ''
     with open(pth_img, 'rb') as f:
@@ -63,6 +71,65 @@ def base64_to_PIL(string):
     except Exception as e:
         print(e)
         return None
+
+def scale_x(_cords, resize_x):
+    xmin, ymin, xmax, ymax = _cords
+    xmid = (xmin + xmax) / 2
+    xmin_ = math.ceil(xmid - resize_x * (xmid - xmin))
+    xmax_ = math.floor(xmid + resize_x * (xmax - xmid))
+
+    return xmin_, ymin, xmax_, ymax
+
+
+def scale_y(_cords, resize_y):
+    xmin, ymin, xmax, ymax = _cords
+    ymid = (ymin + ymax) / 2
+    ymin_ = math.ceil(ymid - resize_y * (ymid - ymin))
+    ymax_ = math.floor(ymid + resize_y * (ymax - ymid))
+
+    return xmin, ymin_, xmax, ymax_
+
+def draw_box(cords, pth_img, pth_img_rect, color=(0, 0, 255), resize_x=1.0, thickness=1, text='', seqnum=False,
+             hidebox=False, show_img=False):
+    try:
+        # img = cv2.imread(pth_img)
+        img = cv_imread(pth_img)  # 解决中文路径文件的读
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        draw_1 = img
+
+        boxes = []
+
+        for cord in cords:
+            _cord = int(cord[0]), int(cord[1]), int(cord[4]), int(cord[5])
+            xmin, ymin, xmax, ymax = _cord
+            if resize_x < 1.0:
+                _cord = scale_x(_cord, resize_x)
+            boxes.append(_cord)
+
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        for ibox, box in enumerate(boxes):
+            x, y, x_, y_ = box
+            # draw_1 = cv2.rectangle(img, (x,y), (x_,y_), (0,0,255), 2 )
+            thick = thickness if not hidebox else 0
+
+            draw_1 = cv2.rectangle(img, (x, y), (x_, y_), color, thick)
+            if seqnum:
+                draw_1 = cv2.putText(img, str(ibox), (int((x + x_) / 2 - 10), y + 20), font, 0.6, color=color)
+            if not '' == text:
+                font = cv2.FONT_HERSHEY_SIMPLEX
+                draw_1 = cv2.putText(img, text, (int((x + x_) / 2 - 10), y + 16), font, 0.4, color=color)
+
+        # print('Writing to image with rectangle {}\n'.format(pth_img_rect))
+        cv_imwrite(pth_img_rect, draw_1)  # 解决中文路径文件的写
+        if show_img:
+            cv2.imshow('img', draw_1)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
+    except Exception as e:
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+        print(exc_type, e.__str__(), ',File', fname, ',line ', exc_tb.tb_lineno)
+        traceback.print_exc()  # debug.error(e)
 
 def test_one(pth_img):
     img = readPILImg(pth_img)
@@ -99,9 +166,63 @@ def test_one(pth_img):
 
     img = readPILImg(pth_img)
 
+    res4api_detect_line = request_api(url_page_recog_0, param_recog, _auth)
+
+    res_detect_line = {
+        int(itm['name']): {'box': [float(pt) for pt in itm['box']], 'text': itm['text']} for itm in res4api_detect_line
+    }
+    out_sav = ''
+    cords = [v['box'] for index, v in res_detect_line.items()]
+    print(cords)
+
+    pth_img_rect = os.path.join(pth_sav_dir, filename + 'rec.jpg')
+    draw_box(cords, pth_img, pth_img_rect, resize_x=0.8)
+
+    # print(res_detect_line.get('text'))
+    for box in res4api_detect_line:
+        print(box.get('text'))
+
+
+# faster-rcnn char detect
+def test_char_detect_1(pth_img):
+
+    api_base_url = 'http://api.chinesenlp.com:7001'
+    AUTH = ('jihe.com', 'DIY#2020')
+
+    # url_char_detect = '{}/ocr/v1/char_detect_1'.format(api_base_url)
+    url_char_detect = '{}/ocr/v1/char_detect'.format(api_base_url)
+    img = readPILImg(pth_img)
+
+    filename, file_ext = os.path.splitext(os.path.basename(pth_img))
+    pth_dir = os.path.abspath( os.path.dirname(pth_img) )
+    pth_sav_dir = os.path.join(pth_dir,'output')
+    if not os.path.exists(pth_sav_dir):
+        os.makedirs(pth_sav_dir)
+
+    imgbase64 = base64of_img(pth_img)
+    param_detect = {
+        'picstr': imgbase64,
+        'mod': 'base64',
+        'do_ocr': 1
+    }
+    r = requests.post(url_char_detect, data=param_detect, auth=AUTH)
+    str_res = r.text
+    o_res = json.loads(str_res)
+    res4api_detect_char = o_res['data']
+    # 重组
+    res_detect_char = {
+        int(itm['name']):[float(pt) for pt in itm['box']] for itm in res4api_detect_char
+    }
+    print(res_detect_char)
+    cords = [v for index,v in res_detect_char.items()]
+    # 绘矩形框
+    pth_img_rect = os.path.join(pth_sav_dir,filename+'rec.jpg')
+    draw_box(cords, pth_img, pth_img_rect,show_img=True)
 
 
 
+test_one('/Users/Beyoung/Desktop/Projects/ER/dataset/ER007/20_19584_jpg/000009.jpg')
+# test_char_detect_1('/Users/Beyoung/Desktop/Projects/ER/dataset/ER007/20_19584_jpg/000009.jpg')
 
-res4api_detect_line = request_api(url_page_recog_0, param_recog, _auth)
+
 
