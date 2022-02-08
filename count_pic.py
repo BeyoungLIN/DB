65c0ab3d3baf2dfaa5bec0351355409f4e05f3b7
beyoung <linbeyoung@foxmail.com>
2022-02-08 17:51:28 +0800

    update read_and_batch_run

--- count_pic.py
+++ count_pic.py
@@ -8,16 +8,18 @@ done_file_txt_list = [
     # '/disks/sdb/euphoria/deep-text-recognition-benchmark/extract/todo_list_1.txt',  # 第一批 x-12.1 60w
     # '/disks/sdb/euphoria/deep-text-recognition-benchmark/extract/todo_list_2.txt',  # 第二批 12.1-
     # '/disks/sdb/euphoria/deep-text-recognition-benchmark/extract/todo_list_3.txt',  # 第三批 12.20-
-    '/disks/sdb/euphoria/deep-text-recognition-benchmark/extract/todo_list_1222_1.txt',  # 第一批 12.22
+    # '/disks/sdb/euphoria/deep-text-recognition-benchmark/extract/todo_list_1222_1.txt',  # 第一批 12.22
 
     # 文件夹
     # '/disks/sdb/euphoria/pkg/seg_detector/extract/done_folder_2.txt'
     # '/disks/sdb/euphoria/pkg/seg_detector/extract/done_folder_3.txt',  # 第三批文件夹
+    '/disks/sdb/euphoria/pkg/seg_detector/extract/done_folder_1222_1.txt'  # 第四批文件夹
 ]
 count = 0
 for file in done_file_txt_list:
     with open(file, 'r', encoding='utf-8') as f:
         lines = f.readlines()
+    print(file)
     count += len(lines)
 print(count)
 # print(count)
